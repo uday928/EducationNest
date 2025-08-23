2c145bee2070696a9deb656377a9c3500b05b579
uday928 <udaygandhi2811@gmail.com>
2025-08-23 11:35:18 +0530

    23-08-2025 comm-2

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-gray-900 md:px-36 text-left w-full mt-10'>
+      <div className='flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border-b border-white/30'>
+          <div className='flex flex-col md:items-start items-center w-full '>
+            <img src={assets.logo_dark} alt="logo" />
+            <p className='mt-6 text-center md:text-left text-sm text-white/80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quibusdam neque sunt corporis dolore voluptatem consequatur ad quasi voluptatibus non.</p>
+
+          </div>
+          <div className='flex flex-col md:items-start items-center w-full'>
+            <h2 className='font-semibold text-white mb-5'>Company</h2>
+            <ul className='flex md:flex-col w-full justify-between text-sm text-white/80 md:space-y-2'>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Contact us</a></li>
+              <li><a href="#">Privecy Policy</a></li>
+            </ul>
+          </div>
+          <div className='hidden md:flex flex-col items-start w-full '>
+            <h2 className='font-semibold text-white mb-5'>Subscribe to our site</h2>
+            <p className='text-sm text-white/80'>Latest updates will be shared via E-mail</p>
+            <div className='flex items-center gap-2 pt-4'>
+              <input type="email" placeholder='Enter your email' className='border border-gray-500/30 bg-gray-300 text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm'/>
+              <button className='bg-blue-600 w-24 h-9 text-white rounded '>Subscribe</button>
+            </div>
+          </div>
+      </div>
+      <p className='py-4 text-center text-xs md:text-sm text-white/60'>From Uday Gandhi #UG</p>
+    </footer>
   )
 }
 
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -3,6 +3,8 @@ import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
 import CoursesSection from '../../components/student/CoursesSection'
 import TestimonialSection from '../../components/student/TestimonialSection'
+import CallToAction from '../../components/student/CallToAction'
+import Footer from '../../components/student/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
       <Companies/>
       <CoursesSection/>
       <TestimonialSection/>
+      <CallToAction/>
+      <Footer/>
     </div>
   )
 }

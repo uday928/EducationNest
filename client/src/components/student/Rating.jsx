ad4d1e1dcc4e40faa7758345b5d966d1c4baeb58
uday928 <udaygandhi2811@gmail.com>
2025-08-24 11:22:28 +0530

    24-08-2025 comm-3 student_done

--- client/src/components/student/Rating.jsx
+++ client/src/components/student/Rating.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Rating = () => {
+const Rating = ({initialRating, onRate}) => {
+  const [rating,setRating]=useState(initialRating || 0)
+
+  const handleRating=(value)=>{
+    setRating(value);
+    if(onRate) onRate(value)
+  }
+
+  useEffect(()=>{
+    if(initialRating){
+      setRating(initialRating)
+    }
+  },[initialRating])
+
   return (
-    <div>Rating</div>
+    <div>
+      {Array.from({length: 5},(_,index)=>{
+        const starValue=index+1;
+        return (
+          <span onClick={()=>handleRating(starValue)} key={index} className={`text-xl sm:text-2xl cursor-pointer transition-colors 
+            ${starValue <= rating ? 'text-yellow-500' : 'text-gray-400'}`}>&#9733;</span>
+        )
+      })}
+    </div>
   )
 }
 
